bac51838ad6025c005da7ab8f76bf28a95258e16
Jamie <ScruffyTheMoose@users.noreply.github.com>
2022-01-20 17:42:14 -0600

    init render

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
 function setup() {
-    createCanvas(800, 500)
+    createCanvas(windowWidth, windowHeight);
+
+    // midpoints of x and y axis
+    x_mid = windowWidth / 2;
+    y_mid = windowHeight / 2;
+
+    // quarters of x and y axis
+    x_quart = windowWidth / 4;
+    y_quart = windowHeight / 4;
+
 }
 
+
 function draw() {
-    background(225)
-    draw(30, 30, 20)
+    // to be made dynamic
+    background(220);
+
+    // rendering center line to split the canvas
+    line(x_mid, 0, x_mid, windowHeight)
+
+    // rendering arc to right side of centerline
+    arc(x_mid, y_mid, x_quart, y_quart, -HALF_PI, HALF_PI)
+
 }
